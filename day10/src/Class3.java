267a14d9a6b574ae66581eac7e610ffd1db1395f
ravensubham <choudhurysubham2708@gmail.com>
2022-08-08 23:31:09 +0530

    first commit

---
+++ day10/src/Class3.java
@@ -0,0 +1,18 @@
+
+public class Class3 {
+	public static void main(String[] args) {
+
+		String s = "friends";
+
+		int x = 0;
+
+		while(x<3){
+
+		System.out.print(s.charAt(x));
+
+		x++ ;
+
+		}
+
+		}
+}

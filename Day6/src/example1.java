fb8a1bae622c39f15d5a47a891804f723045bc7f
shubham raj <71871642+ravensubham@users.noreply.github.com>
2022-09-29 13:47:40 +0530

    first commit

---
+++ Day6/src/example1.java
@@ -0,0 +1,12 @@
+
+public class example1 {
+
+	public static void main(String[] args) {
+		int num=567;
+		double num1=num;
+		System.out.println(num1);
+		double abc=55.55555;
+		int num3=(int)abc;
+		System.out.println(num3);
+	}
+}
---
+++ Day6/src/foreachloop.java
@@ -0,0 +1,13 @@
+
+public class foreachloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int arr[] = {10,20,30,40};
+     
+     for(int a: arr) {
+    	 System.out.print(a+" ");
+     }
+	}
+
+}

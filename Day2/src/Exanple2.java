aeab6bded70264df1edd44b2e6a6d6ac24a804bd
shubham raj <71871642+ravensubham@users.noreply.github.com>
2022-09-29 13:45:41 +0530

    first commit

---
+++ Day2/src/Exanple2.java
@@ -0,0 +1,14 @@
+
+public class Exanple2 {
+	public static void main(String[] args) {
+
+		int myInt = 9;
+
+		double myDouble = myInt;
+
+		System.out.println(myDouble);
+
+		System.out.println(myInt);
+
+		}
+}

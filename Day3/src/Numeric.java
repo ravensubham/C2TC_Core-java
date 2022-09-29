49f638b245628dd21cc417e77515bcdb25a8762a
shubham raj <71871642+ravensubham@users.noreply.github.com>
2022-09-29 13:46:46 +0530

    first commit

---
+++ Day3/src/Numeric.java
@@ -0,0 +1,14 @@
+
+public class Numeric {
+
+	public static void main(String[] args) {
+		int x = 10;
+		int y = 20;
+		
+		if(x+y > 10) {
+		System.out.print("x+y is greater than 10");	
+		}
+
+	}
+
+}
